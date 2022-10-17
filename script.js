bc32d98254996eff9ca77695c92b26f41e2ec96f
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-10-16 23:40:46 -0300

    teste

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const input = document.getElementById("input")
 const btnInput = document.getElementById("btnAdd")
 const taskBox = document.getElementById("task-box")
+const div= document.getElementById("task-item")
 
 function addTarefa() {
   let valueTask = input.value;
-
   if (valueTask !== "") {
-
     let item = document.createElement("div");
     item.innerHTML = valueTask;
     item.className = "task-item";
     taskBox.appendChild(item);
     input.value = "";
   }
+}
 
+function concluiTarefa() {
+  div.classList.toggle("task-item-ok");
 }
 
 btnInput.addEventListener ("click", addTarefa);
+div.addEventListener ("click", concluiTarefa);
