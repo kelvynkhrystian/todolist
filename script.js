934e66fccef647b271c5404dac3d1baf38dc40bd
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-10-23 22:19:32 -0300

    ajuste-css

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const input = document.getElementById("input")
 const btnInput = document.getElementById("btnAdd")
 const taskBox = document.getElementById("task-box")
+const li = document.getElementsByTagName("li")
 
 function addTarefa() {
   let valueTask = input.value;
   if (valueTask !== "") {
     let item = document.createElement("li");
     item.innerHTML = valueTask;
-    item.className = "task-item";
+    item.className = "taskItem";
     taskBox.appendChild(item);
     input.value = "";
   }
 }
 
-const li = document.getElementsByClassName("task-item")
-
-function concluiTarefa() {
-  let itemOK = li
-  itemOK.classList.toggle("task-item-ok");
-  console.log("ok")
-}
+// function concluiTarefa() {
+//   let itemOK = li
+//   itemOK.classList.toggle("task-item-ok");
+//   console.log("ok")
+// }
 
 btnInput.addEventListener ("click", addTarefa);
-li.addEventListener ("dblclick", concluiTarefa);
+// li.addEventListener ("click", concluiTarefa);
+console.log(li)
