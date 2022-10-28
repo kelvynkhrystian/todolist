23eb767e071e9f8768631a5213ce2dc649136528
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2022-10-28 17:47:22 -0300

    add-btn-clear-html-css

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const input = document.getElementById("input")
 const btnInput = document.getElementById("btnAdd")
+const btnClear = document.getElementById("btnClearAll")
 const taskBox = document.getElementById("task-box")
 const li = document.getElementsByTagName("li")
 
@@ -17,6 +18,7 @@ const addTask = () => {
     item.ondblclick = removeTask;
     taskBox.appendChild(item);
     input.value = "";
+    btnClear.style.display = 'block'
   }
 }
 
@@ -28,7 +30,24 @@ const removeTask = event => {
   event.target.remove();
 }
 
+const clearAll = () => {
+
+  console.log(taskBox.children);
+  console.log(" ");
+  console.log(" ");
+
+  for (let i=0; i<=taskBox.children.length; i+=1) {
+    let itemLi = taskBox.children[i]
+    // console.log(taskBox.children[i].innerHTML);
+    // console.log(itemLi);
+    // taskBox.removeChild(itemLi)
+    itemLi.innerHTML = ''
+  }
+
+  // btnClear.style.display = 'none'
+}
+
 // Evento principal
 
 btnInput.addEventListener ("click", addTask);
-
+btnClear.addEventListener ("click", clearAll);
